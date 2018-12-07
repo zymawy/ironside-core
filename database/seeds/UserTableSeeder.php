6e9d75b1fe005afccc752c262963cf9b571162f5
Hamza Zymawy <zymawy@gmail.com>
2018-12-07 04:03:39 +0300

    Fixed And Cleanup

--- database/seeds/UserTableSeeder.php
+++ database/seeds/UserTableSeeder.php
@@ -5,11 +5,14 @@ use Zymawy\Ironside\Commands\InstallCommand;
 use Carbon\Carbon;
 use App\Role;
 use Illuminate\Database\Seeder;
-
+use DB;
 class UserTableSeeder extends Seeder
 {
     public function run(Faker\Generator $faker = null)
     {
+      DB::statement('SET FOREIGN_KEY_CHECKS=0;');
+        User::truncate();
+      DB::statement('SET FOREIGN_KEY_CHECKS=1;');
 
         //-------------------------------------------------
         // Default Dashboard
